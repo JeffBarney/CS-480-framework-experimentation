8bad9326633878e14928d63aa5662a617f28aadf
jeffersonbarney <jefferson.barney@icloud.com>
2020-10-05 17:23:47 -0600

    Adds react app and hopefully adds navigation to it from angular

---
+++ react/my-react-app/build/precache-manifest.ffb313412de76ef88d0796ed8caea939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0bef2763d35bcd382a5f2656ef0421",
+    "url": "/~bjeffer/react/index.html"
+  },
+  {
+    "revision": "0eeb7307618538de62d4",
+    "url": "/~bjeffer/react/static/css/main.b792a898.chunk.css"
+  },
+  {
+    "revision": "e45b2938c302e62d8456",
+    "url": "/~bjeffer/react/static/js/2.c419dce7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/~bjeffer/react/static/js/2.c419dce7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eeb7307618538de62d4",
+    "url": "/~bjeffer/react/static/js/main.036a31e8.chunk.js"
+  },
+  {
+    "revision": "d6da1edd6efbe25ef262",
+    "url": "/~bjeffer/react/static/js/runtime-main.2e53ef58.js"
+  }
+]);
